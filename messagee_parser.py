31247379153e80f3b1a990a5925552baa64c8f61
laverina <averina.lyubov@gmail.com>
2021-11-22 20:45:06 +0300

    added simple training parsing

---
+++ messagee_parser.py
@@ -0,0 +1,17 @@
+def parse_training(text):
+    lines = text.split('\n')
+    training = []            # training = list of exercises
+    next_exercise = []       # each exercise is a list of params (name, details)
+    for index, line in enumerate(lines):
+        line = lines[index].strip().lower()
+        if index == 0:
+            pass  # skip first line - it's command
+        elif line == '':
+            training.append(next_exercise)
+            next_exercise = []
+        else:
+            next_exercise.append(line)
+    if next_exercise:
+        training.append(next_exercise)
+
+    return training
